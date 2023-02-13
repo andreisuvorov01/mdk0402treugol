43d6a67b3d2643f5a5b52a055a71151c68058dea
dron2309 <andrei03jump@gmail.com>
2023-02-13 19:30:15 +0300

    практика мдк0402

---
+++ treugolnik_mdk0402/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace treugolnik_mdk0402
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите 1 сторону");
+            Int32 a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите 2 сторону");
+            Int32 b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите 3 сторону");
+            Int32 c = Convert.ToInt32(Console.ReadLine());
+
+            if (a + b > c && a + c > b && b + c > a)
+            {
+                Console.WriteLine("Треугольник возможен");
+            }
+            else
+                Console.WriteLine("Треугольник невозможен");
+        }
+    }
+}
